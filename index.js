008b9f7b94e09d7664b52cea9126a9a732048089
Rodger Combs <rodger.combs@gmail.com>
2013-01-13 23:00:49 -0600

    Added RS232 support

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
 var events = require("events"),
-	net = require("net");
+	net = require("net"),
+	SerialPort = require("serialport").SerialPort;
 
 var exports = module.exports = function(ip, port, callback){
 	var self = this;
-	var client = this.client = new net.Socket();
-	client.on("error", function(err) {
-		self.emit("error", err);
-	});
-	client.on("data", function(data) {
-		self.emit("data", data);
-	});
-	client.connect(port, ip, function() {
-		self.emit("connected");
-	});
+	this.mode = "NET";
+	if(typeof port == "object" || !port){
+		this.mode = "RS232";
+	}
+	if(callback){
+		this.addEventListener("connected", callback);
+	}
+	if(this.mode == "NET"){
+		this.socket = new net.Socket();
+		this.socket.connect(port, ip, function() {
+			self.emit("connected");
+		});
+		this.setupEvents();
+	}else if(!ip){
+		SerialPort.list(function(list){
+			if(!list || !list[0]){
+				throw new Error("No serial ports exist!");
+			}
+			this.setupRS232(list[0], port);
+		});
+	}else{
+		this.setupRS232(ip, port);
+	}
 };
 
 exports.prototype = new events.EventEmitter();
 
-exports.prototype.raw = function(command) {
+exports.prototype.setupRS232 = function(port, options){
+	this.socket = new SerialPort(port, options, function(){
+		self.emit("connected");
+	});
+}
+
+exports.prototype.setupEvents = function(){
+	this.socket.on("error", function(err) {
+		self.emit("error", err);
+	});
+	this.socket.on("data", function(data) {
+		self.emit("data", data);
+	});
+}
+
+exports.prototype.writeRaw = function(data){
+	this.socket.write(data);
+}
+
+exports.prototype.writeWithChecksum = function(command) {
 	command += "\\\\";
 	var checksum = 0;
 	for(var i = 0; i < command.length; i++){
 		checksum += command.charCodeAt(i);
 	}
-	this.client.write(command + (checksum % 256).toString(16) + "\r\n");
+	this.writeRaw(command + (checksum % 256).toString(16) + "\r\n");
 };
 
 exports.prototype.disconnect = function(){
@@ -31,16 +64,16 @@ exports.prototype.disconnect = function(){
 };
 
 exports.prototype.writeTab = function(message, descriptor, templateDataArray, id) {
-	this.raw((id ? ("w\\" + id) : "W") + "\\" + message + "\\" + descriptor + "\\" + templateDataArray.join("\\"));
+	this.writeWithChecksum((id ? ("w\\" + id) : "W") + "\\" + message + "\\" + descriptor + "\\" + templateDataArray.join("\\"));
 };
 
 exports.prototype.writeField = function(message, field, data, id) {
-	this.raw((id ? ("u\\" + id) : "U") + "\\" + message + "\\" + field + "\\" + data);
+	this.writeWithChecksum((id ? ("u\\" + id) : "U") + "\\" + message + "\\" + field + "\\" + data);
 };
 
 exports.updateOnAirMessage = function(message, templateDataArray, buffer) {
 	if (!buffer) {
 		buffer = 1;
 	}
-	return raw("V\\5\\13\\1\\" + buffer + "\\" + message + "\\1\\" + templateDataArray.join("\\"));
+	this.writeWithChecksum("V\\5\\13\\1\\" + buffer + "\\" + message + "\\1\\" + templateDataArray.join("\\"));
 };
